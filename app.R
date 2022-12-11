67ea7cf484b28c6ba9028c26e60b8458d727b1a4
jtsang <56046208+letsang@users.noreply.github.com>
2022-12-11 21:11:44 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,170 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Shiny
+library(shiny)
+library(shinyjs)
+library(shinyWidgets)
+# Time series & Forecast
+library(forecast)
+# Data Visualization
+library(ggplot2)
+library(shiny)
+library(shinyjs)
+library(shinyWidgets)
+library(htmltools)
+# Data Wrangling
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(aweek)
+library(stringr)
+library(rebus)
+# Data Visualization
+library(ggplot2)
+library(dygraphs)
+library(plotly)
+# Table
+library(DT)
+library(formattable)
+library(RCurl)
+# Office365
+library(Microsoft365R)
+
+### NETWORK ###
+# https://frankzheng.me/en/2022/06/run-shiny-apps-locally/
+x <- system("ipconfig", intern=TRUE)
+z <- x[grep("IPv4", x)]
+ip <- gsub(".*? ([[:digit:]])", "\\1", z)
+port <- 8888
+
+### ONE DRIVE ###
+odb <- get_business_onedrive()
+items <- odb$list_items("planR/seasonality")
+
+### UI FONT CONSTANTS
+t <- list(
+  family = "Helvetica",
+  size = 10,
+  color = "black")
+
+t1 <- list(
+  family = "Helvetica",
+  size = 8,
+  color = "black")
+
+### DATABASE ###
+base <- readRDS("base.rds")
+
+### SERVER ###
+server <- function(input, output, session) {
+  
+  values <- reactiveValues()
+  
+  observe({
+    values$smc <- input$smc
+    # updateSelectizeInput(session, "smc", choices = unique(base$SMC[base$SMC %in% input$dep]))
+  })
+  
+  ### UPDATE PACKSHOT ###
+  output$picture <- renderUI({
+    htmltools::tags$img(src=paste0("https://getmedia.pdi.keringapps.com/image/pijqmiaszv/", values$smc), height="200px")
+  })
+
+  res <- reactive({
+    tmp <- base %>% filter(SMC == input$smc) %>% mutate(AWS = 0, Forecast = 0, WOC = 4, Closing = 0, Need = 0)
+    ### AWS ###  
+    for(i in 1:nrow(tmp)){
+      if(i %in% 1:4){
+        tmp$AWS[i] <- 0
+      }else{
+        tmp$AWS[i] <- mean(c(tmp$Sales[i-1], tmp$Sales[i-2], tmp$Sales[i-3], tmp$Sales[i-4]))
+      }
+    }
+    
+    ### FORECAST & NEED ###
+    for(i in 1:nrow(tmp)){
+      if(tmp$Week[i] < 50 & tmp$Year[i] <= 2022){
+        tmp$Forecast[i] <- 0
+        tmp$Closing[i] <- 0
+        tmp$Need[i] <- 0
+      }else{
+        tmp$Forecast[i] <- round(tmp[tmp$Week == 50 & tmp$Year == 2022, "AWS"]*52*tmp$Weight[i])
+        tmp$Closing[i] <- round(max(0,(tmp$Closing[i-1] + tmp$OH[i] + tmp$PFP[i] - tmp$Forecast[i])))
+        tmp$Need[i] <- round(min(0,(tmp$Closing[i] - tmp$Forecast[i] - tmp$Forecast[i+1] - tmp$Forecast[i+2] - tmp$Forecast[i+3]))*-1)
+      }
+    }
+    tmp$Time <- factor(tmp$Time, levels = unique(tmp$Time), ordered = TRUE)
+    tmp %>% select(Time, Week, Year, Weight, SMC, Department, Line, Season, OH, PFP, Sales, AWS, WOC, Forecast, Closing, Need)
+  })
+  
+  output$table = DT::renderDataTable({
+    datatable(res(),
+              options = list(searching = FALSE, paging = FALSE, columnDefs = list(list(className = 'dt-center', targets = "_all"))),
+              extensions = c('Scroller'),
+              rownames = FALSE,
+              editable = "cell",
+              escape = FALSE)
+  })
+  
+  output$plot <- renderPlotly({
+    
+    plot_ly(res()) %>% 
+      add_trace(x = ~Time, y = ~OH, type = "bar", name ="OH") %>%
+      add_trace(x = ~Time, y = ~Closing, type = "bar", name ="Stock Proj") %>%
+      add_trace(x = ~Time, y = ~Need, type = "bar", name ="Reorder") %>%
+      add_trace(x = ~Time, y = ~Forecast, type = "scatter", name ="Forecast", mode='markers+lines', yaxis = "y2", marker = list(color = "red"), opacity=1) %>%
+      add_trace(x = ~Time, y = ~Sales, type = "scatter", name ="Sales", mode='markers+lines', yaxis = "y2", marker = list(color = "blue"), opacity=1) %>%
+      layout(xaxis = list(title = list(text=""), dtick=1, tickfont = list(size = 8), font = t),
+             yaxis = list(title = list(text=""), tickfont = list(size = 8), font = t, rangemode = "nonnegative"),
+             yaxis2 = list(title = list(text=""), tickfont = list(size = 8), font = t, rangemode = "nonnegative", overlaying = "y", side = "right"),
+             margin = list(r = 50),
+             showlegend = FALSE)
+    
+  })
+  
+}
+
+### CLIENT ###
+ui <- navbarPage(
+  htmltools::tags$img(src = "https://www.ysl.com/on/demandware.static/-/Library-Sites-Library-SLP/default/dw86be354f/images/logo.svg", height = "20px"),
+  tabPanel("★",
+           fluidPage(
+             htmltools::tags$style(HTML("body {font-family:'Helvetica',sans-serif; font-size:10px
+                                              }
+             
+                                        .js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {
+                                                  background: black;
+                                                  border-top: 1px solid black;
+                                                  border-bottom: 1px solid black;
+                                                  }
+
+                                        .irs-from, .irs-to, .irs-single { 
+                                                  background: black
+                                                  }"
+             )
+             ),
+             fluidRow(
+               column(6,
+                      selectizeInput("dep", "Department :", choices = unique(base$Department), selected = "HANDBAGS", multiple = TRUE),
+                      selectizeInput("smc", "Style Material Color :", choices = unique(base$SMC), selected = "698651AAANG1000", multiple = FALSE, options = list(maxOptions = 10)),
+                      selectizeInput("weight", "Seasonality :", choices = items$name, selected = items$name[1], multiple = FALSE)
+                      ),
+               column(6, uiOutput("picture")),
+               column(12, plotlyOutput("plot")),
+               column(12, DT::dataTableOutput("table"))
+             ),
+           )
+  )
+)
+
+# Run the application 
+app <- shinyApp(ui = ui, server = server)
+runApp(app, launch.browser = FALSE, port = port, host = ip)
---
+++ base.R
@@ -0,0 +1,88 @@
+
+# Data Wrangling
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(aweek)
+library(stringr)
+library(rebus)
+
+### FILE SOURCE ###
+seasonality <- read_excel("W:\\Business Intelligence\\LIFESTYLE\\10. Forecast & Reorder\\SEASONALITY\\Lifestyle Seasonality 2111.xlsx", skip = 3)
+calendar <- data.frame("Week" = c("Wk. 1 January 2022","Wk. 2 January 2022","Wk. 3 January 2022","Wk. 4 January 2022","Wk. 5 January 2022","Wk. 1 February 2022","Wk. 2 February 2022","Wk. 3 February 2022","Wk. 4 February 2022","Wk. 1 March 2022","Wk. 2 March 2022","Wk. 3 March 2022","Wk. 4 March 2022","Wk. 1 April 2022","Wk. 2 April 2022","Wk. 3 April 2022","Wk. 4 April 2022","Wk. 5 April 2022","Wk. 1 May 2022","Wk. 2 May 2022","Wk. 3 May 2022","Wk. 4 May 2022","Wk. 1 June 2022","Wk. 2 June 2022","Wk. 3 June 2022","Wk. 4 June 2022","Wk. 1 July 2022","Wk. 2 July 2022","Wk. 3 July 2022","Wk. 4 July 2022","Wk. 5 July 2022","Wk. 1 August 2022","Wk. 2 August 2022","Wk. 3 August 2022","Wk. 4 August 2022","Wk. 1 September 2022","Wk. 2 September 2022","Wk. 3 September 2022","Wk. 4 September 2022","Wk. 1 October 2022","Wk. 2 October 2022","Wk. 3 October 2022","Wk. 4 October 2022","Wk. 5 October 2022","Wk. 1 November 2022","Wk. 2 November 2022","Wk. 3 November 2022","Wk. 4 November 2022","Wk. 1 December 2022","Wk. 2 December 2022","Wk. 3 December 2022","Wk. 4 December 2022","Wk. 5 December 2022"),
+                       "Week#" = 1:53)
+
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
+
+sales2 <- read_excel("db\\STOCK PRO - SALES.xlsx", skip = 6) %>%
+  mutate(SMC = paste0(`Style Color MODEL_CODE`,`Style Color MATERIAL_CODE`, `Style Color COLOR_CODE`)) %>%
+  select(SMC,
+         Department = `Department DESC`,
+         Line = `KPI 1`,
+         Season = `KPI 18`,
+         Year,
+         Week = `Week of Year Number`,
+         Sales = `Sales FP (Qty)`)
+sales2[is.na(sales2)] <- 0
+
+inv2 <- read_excel("db\\STOCK PRO - INV.xlsx", skip = 5) %>%
+  merge(calendar, by="Week") %>%
+  select(SMC = `Style Color CODE`,
+         Department = `Department DESC`,
+         Line = `Kpi 1`,
+         Season = `Kpi 18`,
+         Year,
+         Week = Week.,
+         OH) %>%
+  as_tibble()
+inv2[is.na(inv2)] <- 0
+
+pfp2 <- read_excel("db\\STOCK PRO - PFP.xlsx", skip = 4) %>%
+  mutate(SMC = paste0(`Style Color MODEL_CODE`,`Style Color MATERIAL_CODE`, `Style Color COLOR_CODE`)) %>%
+  mutate(Year = year(`Start Delivery Window`), Week = week(`Start Delivery Window`)) %>%
+  select(SMC,
+         Department = `Department DESC`,
+         Line = `KPI 01`,
+         Season = `KPI 18`,
+         Year,
+         Week,
+         PFP = `Pending From Production  (Qty)`) %>%
+  filter(SMC != "NANANA") %>%
+  group_by(SMC, Department, Line, Season, Year, Week) %>%
+  summarise(PFP = sum(PFP))
+pfp2[is.na(pfp2)] <- 0
+
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
+
+### SEASONALITY ###
+seasonality <- seasonality[1:52, c(3,22)]
+names(seasonality)[names(seasonality) == "WEEK #...3"] <- "Week"
+names(seasonality)[names(seasonality) == "SEASONAL CURVE 100%"] <- "Weight"
+
+### MAIN DATASET FOR FORECAST ###
+# STEP 1 TEMPLATE 2 COMPLETE YEARS WITH SALES / INV / PFP DATA
+w22 <- tibble('Week' = 1:52) %>% mutate('Year' = 2022)
+w23 <- tibble('Week' = 1:52) %>% mutate('Year' = 2023)
+main <- rbind(w22, w23) %>% merge(seasonality, by='Week')
+
+# ### C'EST TROP LENT // TROUVER UNE AUTRE METHODE ###
+tmp <- list()
+for(i in 1:length(unique(inv2$SMC))) {
+  tmp[[i]] <- full_join(main, filter(inv2, SMC == inv2$SMC[i]), by = c("Week" , "Year")) %>%
+    fill(SMC, Department, Line, Season, .direction = "updown")
+}
+
+base <- do.call(rbind, tmp)
+base <- left_join(base, sales2, by = c("Week" , "Year", "SMC", "Department", "Line", "Season"))
+base <- left_join(base, pfp2, by = c("Week" , "Year", "SMC", "Department", "Line", "Season"))
+base <- base %>% arrange(SMC, Year, Week, Department, Line, Season) %>% mutate(Time = paste(Week,Year,sep = "-"))
+base[is.na(base)] <- 0
+
+saveRDS(base, "base.rds")
